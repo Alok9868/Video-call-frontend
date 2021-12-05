fdf8e91d11350a97c26e8ebbabedb6380d1a994f
Alok9868 <82724229+Alok9868@users.noreply.github.com>
2021-12-05 16:04:28 +0530

    REmoving bugs and bugs and bugs

--- src/utils/turn.js
+++ src/utils/turn.js
@@ -4,10 +4,16 @@ let TURNIceServers=null;
 
 export const fetchTurnCredentials =async() => {
     const responseData = await api.getTurnCredentials(); 
-    if(responseData.responseToken?.iceServers)
+    console.log(responseData.token.iceServers);
+    if(responseData.responseToken?.iceServers )
     {   
         TURNIceServers=responseData.responseToken.iceServers;
     }
+    else if(responseData.token?.iceServers)
+    { 
+        TURNIceServers=responseData.token.iceServers;
+    }
+    // console.log(TURNIceServers);
     return TURNIceServers;
 }
 export const getTurnIceServers = async() =>{
